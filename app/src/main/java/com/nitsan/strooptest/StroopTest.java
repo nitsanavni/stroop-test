d4a24bdf25dff748572f52ef07165bb61cd339d4
Nitsan Avni <nitsan@realfacetech.com>
2017-01-04 09:42:28 +0200

    more TODOs

--- app/src/main/java/com/nitsan/strooptest/StroopTest.java
+++ app/src/main/java/com/nitsan/strooptest/StroopTest.java
@@ -9,14 +9,27 @@ import rx.subjects.PublishSubject;
 
 // TODOs
 // - add "repeat" button next to summary view
-// - summary view should be a table
-// - yes! we do care about correctness
-// - how is the summary calculation going? I think it might take into account the time of incorrect
-// responses but not the number
 // - add text to summary view
 // - add a "Trial" class - has a Label (congruent or not), start/end time, clicked color, is correct/incorrect
+// - app flow:
+//   - enter child's name (could be done directly in the email app)
+//   - phase 1 - color names written in black - explanation & test - click according to written name
+//   - phase 2 - colored rectangles - explanation & test - click according to
+//   - phase 3 - pseudo-random color names written in pseudo-random colors - explanation & test - click according to color (not name)
+//   - celebration & congrats
+//   - send summary logs
+// - summary logs: 1. child's name 2. per phase (x3) number of incorrect clicks 3. total test time
+// - no need for congruency validation, but yes need for correctness validation
+// - test ends after constant number of trials
+// - open - add settings? (num of trials per test, email address)
 // - Labels randomization distribution - uniform?
-// - use "view source" on online example (Adobe flash player)
+// - open - how do we know the email address?
+// - open - how to pseudo random? answer - Random with same seed
+// - add new types of labels: black color names, colored rectangles
+// - add new types of tests (or "TestSpecifics"): black color names, colored rectangles
+// - TestSpecifics: correctness criterion, instruction text, Label type,
+// - maybe a test could ask the UI to show its explanation...
+// - measure test time directly (not through individual trials)
 
 class StroopTest {
     private final StroopTestUI ui;
